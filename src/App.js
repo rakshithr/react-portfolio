1334390e4186680c2336d35c2c0ead886a64b2e0
rakshithr <rakshith.atreays@nuvedalearning.com>
2022-01-16 16:38:52 +0530

    Complete website done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { Route, Switch, useLocation } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
+import { lightTheme } from "./components/Themes"
+import GlobalStyle from "./globalStyles"
+
+import Main from "./components/Main";
+import AboutPage from "./components/AboutPage";
+import BlogPage from "./components/BlogPage";
+import MySkillsPage from "./components/MySkillsPage";
+import WorkPage from "./components/WorkPage";
+import { AnimatePresence } from "framer-motion";
+
 function App() {
-  return <div>
-    App File
-    </div>
+
+  const location = useLocation();
+
+  return <>
+
+  
+  <GlobalStyle />
+
+    <ThemeProvider theme={lightTheme}>
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.pathname} >
+          <Route exact path="/" component={Main} />
+          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/experience" component={BlogPage} />
+          <Route exact path="/skills" component={MySkillsPage} />
+          <Route exact path="/projects" component={WorkPage} />
+        </Switch>
+      </AnimatePresence>
+    </ThemeProvider>
+    
+    </>
     
 }
 
---
+++ src/components/MySkillsPage.js
@@ -0,0 +1,123 @@
+import React from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+import {lightTheme} from './Themes';
+import { Design, Develope} from './AllSvgs';
+
+
+import LogoComponent from '../subComponents/LogoComponent';
+import SocialIcons from '../subComponents/SocialIcons';
+import PowerButton from '../subComponents/PowerButton';
+import ParticleComponent from '../subComponents/ParticleComponent';
+import BigTitle from '../subComponents/BigTitlte'
+
+const Box = styled.div`
+background-color: ${props => props.theme.body};
+width: 100vw;
+height:100vh;
+position: relative;
+display: flex;
+justify-content: space-evenly;
+align-items: center;
+`
+
+const Main = styled.div`
+border: 2px solid ${props => props.theme.text};
+color: ${props => props.theme.text};
+background-color: ${props => props.theme.body};
+padding: 2rem;
+width: 30vw;
+margin-top: 20%;
+height: auto;
+z-index:3;
+line-height: 1.5;
+cursor: pointer;
+font-family: 'Ubuntu Mono',monospace;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+&:hover{
+    color: ${props => props.theme.body};
+    background-color: ${props => props.theme.text};
+}
+`
+
+const Title = styled.h2`
+display: flex;
+justify-content: center;
+align-items: center;
+font-size: calc(1em + 1vw);
+${Main}:hover &{
+    &>*{
+        fill:${props => props.theme.body};
+    }
+}
+&>*:first-child{
+margin-right: 1rem;
+}
+`
+
+const Description = styled.div`
+color: ${props => props.theme.text};
+font-size: calc(0.6em + 1vw);
+padding: 0.5rem 0;
+${Main}:hover &{
+   
+        color:${props => props.theme.body};
+    
+}
+strong{
+    margin-bottom: 1rem;
+    text-transform: uppercase;
+}
+ul,p{
+    margin-left: 2rem;
+}
+`
+
+const MySkillsPage = () => {
+    return (
+        <ThemeProvider theme={lightTheme}>
+<Box>
+
+<LogoComponent theme='light'/>
+<SocialIcons theme='light'/>
+<PowerButton />
+<ParticleComponent theme='light' />
+            <Main>
+<Title>
+    <Develope width={40} height={40} /> Full Stack Developer
+</Title>
+<Description>
+I value business or brand for which i'm creating, thus i enjoy bringing new ideas to life.
+</Description>
+<Description>
+<strong>Skills</strong>
+<p>
+    <ul>
+        <li><strong>Markup Languages:</strong> HTML5, CSS3</li>
+        <li><strong>Programming Languages:</strong> PHP, Javascript, Node JS, Express JS, React JS, Vue JS</li>
+        <li><strong>Frameworks:</strong> Codeigniter 3, Custom framework.</li>
+        <li><strong>Database:</strong> Postgresql, Firebase, MySQL, Mongo DB.</li>
+        <li><strong>Version Control Tool:</strong> Git, SVN</li>
+    </ul>
+</p>
+</Description>
+<Description>
+<strong>Tools</strong>
+<p>
+VScode, Github etc.
+</p>
+</Description>
+
+            </Main>
+
+            <BigTitle text="SKILLS" top="80%" right="30%" />
+
+        </Box>
+
+        </ThemeProvider>
+        
+    )
+}
+
+export default MySkillsPage
